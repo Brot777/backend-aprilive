8701caf655eb5122d1aeb79eed755f3043da5f5b
brandon <brandonotzin@gmail.com>
2024-05-29 15:06:30 -0600

    add new protety schema notification

--- src/models/notification.ts
+++ src/models/notification.ts
@@ -2,23 +2,23 @@ import mongoose, { model, Schema } from "mongoose";
 
 const notificationSchema = new Schema(
   {
-   title:{
-    type:String,
-    default:"",
-   },
-   description:{
-    type: String,
-    default:"",
-   },
-   type:{
-    type:String,
-    default:"",
-   },
-   receiverId:{
-    type: Schema.Types.ObjectId,
-    ref:"User",
-    require: true,
-   }
+    title: {
+      type: String,
+      default: "",
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    reference: {
+      type: String,
+      default: "",
+    },
+    receiverId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
   },
   {
     timestamps: false,
