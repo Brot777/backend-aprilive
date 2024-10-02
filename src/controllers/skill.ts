88725b441f60e9620794cda20f75f6c50a65bac4
brandon <branotzin@gmail.com>
2024-10-01 21:21:54 -0600

    update feacture newMessage

--- src/controllers/skill.ts
+++ src/controllers/skill.ts
@@ -13,32 +13,3 @@ export const getSkills = async (req: Request, res: Response) => {
     handleHttp(res, "Error_Get_Skills", error);
   }
 };
-
-export const updateSkills = async (req: Request, res: Response) => {
-  const userId = req.params.userId;
-  const skills = req.body.skills.map(
-    (skill: { _id: string; value: string }) => {
-      return skill._id;
-    }
-  );
-  try {
-    if (!mongoose.Types.ObjectId.isValid(userId)) {
-      return res.status(400).json({ error: "Invalid Id" });
-    }
-    const user = await userModel.findOne({ _id: userId });
-    if (!user) {
-      return res.status(404).json({ error: "404 user not found" });
-    }
-    const userPersonAcconutUpdated = await personAccountModel.findOneAndUpdate(
-      { userId },
-      { skills },
-      {
-        new: true,
-      }
-    );
-
-    res.status(200).json(userPersonAcconutUpdated && req.body.skills);
-  } catch (error) {
-    handleHttp(res, "Error_Update_Skills", error);
-  }
-};
