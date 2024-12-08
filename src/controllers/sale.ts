81ae9dde7b46198ef20a08b95c84e3b038ea2bb4
brandon <brandon@gmail.com>
2024-12-08 15:01:16 -0600

    add new feactures: sales a review

--- src/controllers/sale.ts
+++ src/controllers/sale.ts
@@ -3,8 +3,6 @@ import { Request, Response } from "express";
 import { handleHttp } from "../utils/error.handle";
 import serviceHiringModel from "../models/serviceHiring";
 import mongoose, { Types, ObjectId } from "mongoose";
-import serviceModel from "../models/service";
-import { Service } from "../interfaces/service";
 
 export const getMyBalance = async (req: Request, res: Response) => {
   const authorId = req.userId;
@@ -45,52 +43,60 @@ export const getMyBalance = async (req: Request, res: Response) => {
     handleHttp(res, "Error_Upload_Photo", error);
   }
 };
-export const getSales = async (req: Request, res: Response) => {
-  const authorId = req.params.authorId;
-
-  if (!mongoose.Types.ObjectId.isValid(authorId)) {
-    return res.status(400).json({ error: "invalid author id" });
-  }
-
+export const getMySales = async (req: Request, res: Response) => {
+  const authorId = req.userId;
   const limit = 10;
   const queryPage = req.query.page ? `${req.query.page}` : "1";
   let page = Number(queryPage);
   try {
-    const services: Service[] = await serviceModel
-      .find({ authorId })
-      .select("_id");
-    const serviceIds = services.map((service: Service) => service._id);
 
-    const serviceHirings = await serviceHiringModel
-      .find({ seerviceId: { $in: serviceIds } })
-      .skip((page - 1) * limit)
-      .limit(limit)
-      .select("categories authorId images price money title description")
-      .populate("categories")
-      .populate({
-        path: "authorId",
-        select: "_id name photoUrl",
-        populate: {
-          path: "photoUrl",
-          select: "url",
+    if (!mongoose.Types.ObjectId.isValid(authorId)) {
+      return res.status(400).json({ error: "invalid author id" });
+    }
+    
+    const sales = await serviceHiringModel.aggregate([
+      {
+        $lookup: {
+          from: "services",
+          localField: "serviceId",
+          foreignField: "_id",
+          as: "service",
         },
-      })
-      .populate({
-        path: "images",
-        select: "url", // Especifica el campo que deseas recuperar
-      });
-
-    const totalDocs = await serviceModel.count({ authorId }); //Possible performance improvement: cache the value
+      },
+      { $unwind: "$service" }, // Asegurar que cada pago tenga un servicio relacionado
+      {
+        $match: {
+          "service.authorId": new mongoose.Types.ObjectId(authorId),
+          status: "COMPLETED",
+        },
+      }, // Filtrar por el autor
+      { $project: { _id: 1, paymentId:1, status:1, totalAmount:1, createdAt:1 } },
+      {
+        $facet: {
+          metadata: [
+            { $count: "total" }, // Conteo total de documentos
+          ],
+          data: [
+            { $skip: (page - 1) * limit},
+            { $limit: limit },
+          ],
+        },
+      },
+      
+    ])
+      
+    const totalDocs = sales[0].metadata[0]?.total || 0; //Possible performance improvement: cache the value
     const totalPages = Math.ceil(totalDocs / limit); //Possible performance improvement: cache the value
+    
 
     return res.status(200).json({
-      docs: services,
+      docs:sales[0].data,
       currentPage: page,
       limit,
       totalDocs,
       totalPages,
     });
   } catch (error) {
-    handleHttp(res, "Error_Services_By_Author", error);
+    handleHttp(res, "Error_Get_My_Sales", error);
   }
 };
--- src/routes/sale.ts
+++ src/routes/sale.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { isValidToken } from "../middlewares/verifyToken";
-import { getMyBalance } from "../controllers/sale";
+import { getMyBalance, getMySales } from "../controllers/sale";
 
 const router = Router();
 
 router.get("/getMyBalance", isValidToken, getMyBalance);
+router.get("/getMySales", isValidToken, getMySales);
 
 export { router };
