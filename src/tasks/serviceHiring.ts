ba7add2739e25f9a7e46397c3542c868e1012035
brandon <brandonotzin@gmail.com>
2025-03-05 15:39:54 -0600

    auto market completed parial maked

--- src/tasks/serviceHiring.ts
+++ src/tasks/serviceHiring.ts
@@ -1,12 +1,19 @@
 import cron from "node-cron";
 import { serviceHiringModel } from "../models/serviceHiring";
+import { Service } from "../interfaces/service";
+import { ServiceHiring } from "../interfaces/serviceHiring";
+import balanceTransactionModel from "../models/balanceTransaction";
 // Tarea programada para ejecutarse todos los días a la medianoche
 cron.schedule("0 0 * * *", async () => {
   const deadline = new Date();
   deadline.setDate(deadline.getDate() - 10);
 
   try {
-    const updatedServices = await serviceHiringModel.updateMany(
+    const serviceHirings = await serviceHiringModel.find({
+      status: "pendiente",
+      estimatedDeliveryDate: { $lte: deadline },
+    });
+    const dataServiceHiringUpdated = await serviceHiringModel.updateMany(
       {
         status: "pendiente",
         estimatedDeliveryDate: { $lte: deadline },
@@ -17,31 +24,18 @@ cron.schedule("0 0 * * *", async () => {
         },
       }
     );
-console.log(updatedServices);
-/* 
-    updatedServices.forEach(element => {
-      
-    }); {
-      // Marcar servicio como completado
-      await servicios.updateOne(
-        { _id: servicio._id },
-        { $set: { completado: true } }
-      );
 
-      // Crear la transacción
-      await transacciones.insertOne({
-        servicioId: servicio._id,
-        compradorId: servicio.compradorId,
-        prestadorId: servicio.prestadorId,
-        monto: 100, // Ajusta según el servicio
-        fecha: new Date()
+    serviceHirings.forEach(async (serviceHiring: ServiceHiring) => {
+      const service = serviceHiring?.serviceId as Service;
+      await balanceTransactionModel.create({
+        amount: serviceHiring?.netAmount,
+        increase: true,
+        description: "prestación de servicio",
+        paymentId: serviceHiring?.paymentId,
+        userId: service.authorId,
       });
-
-      console.log(updatedServices);
-      
-    console.log(
-      `Servicios contratados actualizados: ${updatedServices.modifiedCount}`
-    ); */
+    });
+    console.log("se actualizo" + dataServiceHiringUpdated.modifiedCount);
   } catch (error) {
     console.error("Error al actualizar los servicios:", error);
   }
