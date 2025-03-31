b417b60aff1d29b967a7d1ed4aa6d9f487f6d5c7
brandon <brandonotzin@gmail.com>
2025-03-31 15:30:18 -0600

    add route webhook-cancel-subcription test-2

--- src/routes/subcription.ts
+++ src/routes/subcription.ts
@@ -4,12 +4,13 @@ import { isValidToken } from "../middlewares/verifyToken";
 import {
   cancelSubcriptionById,
   getMySubscriptions,
+  webhookCancelSubcription,
 } from "../controllers/subscription";
 
 const router = Router();
 
 router.get("/getMySubcriptions", isValidToken, getMySubscriptions);
 router.post("/cancel-subcription", isValidToken, cancelSubcriptionById);
-router.post("/webhook/cancel-subcription", isValidToken, cancelSubcriptionById);
+router.post("/webhook/cancel-subcription", webhookCancelSubcription);
 
 export { router };
