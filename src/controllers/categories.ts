b991d0f7094ff4caf6b18af5be2e8268866ce651
brandon <brandon@gmail.com>
2025-07-27 13:41:14 -0600

    add feacture-add-many-categories

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -11,3 +11,13 @@ export const getCategories = async (req: Request, res: Response) => {
     handleHttp(res, "Error_Get_Categories", error);
   }
 };
+
+export const addManyCategories = async (req: Request, res: Response) => {
+  const categories = req.body.categories;
+  try {
+    await categoryModel.insertMany(categories);
+    res.status(200).json({ msj: "successfully added" });
+  } catch (error) {
+    handleHttp(res, "Error_Add_Many_Categories", error);
+  }
+};
--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { getCategories } from "../controllers/categories";
+import { addManyCategories, getCategories } from "../controllers/categories";
+import { isValidToken } from "../middlewares/verifyToken";
 const router = Router();
 
 router.get("/", getCategories);
+router.post("/add-many", isValidToken, addManyCategories);
 
 export { router };
