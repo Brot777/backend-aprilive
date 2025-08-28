ffb268f49e51ca7f37b934bfdbd0d8971cd93eff
brandon <brandonotzin@gmail.com>
2025-08-28 16:11:32 -0600

    redesing feacture-auth-with-google

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid";
 import jwt from "jsonwebtoken";
 import { comparePassword, encryptPassword } from "../utils/bcrypt.handle";
 import { LoginUser, RegisterUser } from "../interfaces/user.interface";
@@ -11,7 +12,7 @@ import nodemailer from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 import { getTemplateRestartPassword } from "../config/mail";
 import { OAuth2Client } from "google-auth-library";
-import { GOOGLE_CLIENT_ID } from "../config/OAuth2";
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "../config/OAuth2";
 
 export const registerNewUser = async (user: RegisterUser) => {
   user.password = await encryptPassword(user.password);
@@ -133,7 +134,7 @@ export const sendEmailResetPassword = async (
 };
 
 export const authorizationWithGoogle = async (idToken: string) => {
-  const client = new OAuth2Client(GOOGLE_CLIENT_ID);
+  const client = new OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET);
 
   const ticket = await client.verifyIdToken({
     idToken,
@@ -147,6 +148,8 @@ export const authorizationWithGoogle = async (idToken: string) => {
       status: 401,
     };
   }
+  console.log(payload);
+
   const { sub, email, name, picture } = payload;
 
   // Buscar usuario en tu base de datos o crearlo
@@ -155,62 +158,64 @@ export const authorizationWithGoogle = async (idToken: string) => {
   });
   const userFound = await userModel.findById(personAccountFound?.userId);
 
-  if (!userFound) {
-    const userSaved = await userModel.create({
-      email,
-      name,
-      googleId: sub,
-      avatar: picture,
-    });
-
-    const personAccountSaved = await personAccountModel.create({
-      userId: userSaved._id,
-    });
-    const avatarSaved = await avatarModel.create({
-      userId: userSaved._id,
-    });
-    const presentationVideoSaved = await presentationVideoModel.create({
-      userId: userSaved._id,
-    });
-
-    const freeUserRole = await roleModel.findOne({
-      name: "Free User",
-    });
-
-    const accountTypeSaved = await accountTypeModel.create({
-      userId: userSaved._id,
-      role: freeUserRole?._id,
-    });
-
-    await userModel.findByIdAndUpdate(
-      userSaved._id,
-      {
-        photoUrl: avatarSaved._id,
-        videoUrl: presentationVideoSaved._id,
-        personAccount: personAccountSaved._id,
-        accountType: accountTypeSaved._id,
-      },
-      {
-        new: true,
-      }
-    );
-
-    const token = jwt.sign({ _id: userSaved._id }, process.env.SECRET || "");
+  if (userFound) {
+    const token = jwt.sign({ _id: userFound._id }, process.env.SECRET || "");
     return {
       response: {
-        _id: userSaved._id,
-        username: userSaved.username,
+        _id: userFound._id,
+        username: userFound.username,
         token,
         isCompany: false,
       },
       status: 200,
     };
   }
-  const token = jwt.sign({ _id: userFound._id }, process.env.SECRET || "");
+
+  const userSaved = await userModel.create({
+    email: { value: email },
+    name,
+    username: `${name}_${uuidv4()}`,
+    googleId: sub,
+    avatar: picture,
+  });
+
+  const personAccountSaved = await personAccountModel.create({
+    userId: userSaved._id,
+  });
+  const avatarSaved = await avatarModel.create({
+    userId: userSaved._id,
+  });
+  const presentationVideoSaved = await presentationVideoModel.create({
+    userId: userSaved._id,
+  });
+
+  const freeUserRole = await roleModel.findOne({
+    name: "Free User",
+  });
+
+  const accountTypeSaved = await accountTypeModel.create({
+    userId: userSaved._id,
+    role: freeUserRole?._id,
+  });
+
+  await userModel.findByIdAndUpdate(
+    userSaved._id,
+    {
+      photoUrl: avatarSaved._id,
+      videoUrl: presentationVideoSaved._id,
+      personAccount: personAccountSaved._id,
+      accountType: accountTypeSaved._id,
+    },
+    {
+      new: true,
+    }
+  );
+
+  const token = jwt.sign({ _id: userSaved._id }, process.env.SECRET || "");
   return {
     response: {
-      _id: userFound._id,
-      username: userFound.username,
+      _id: userSaved._id,
+      username: userSaved.username,
       token,
       isCompany: false,
     },
