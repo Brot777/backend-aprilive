7ba9053c4b838595cf4be3aab32f6341f0b42174
brandon <brandonotzin@gmail.com>
2025-09-03 16:00:01 -0600

    delete fuction update email string to object

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import { createInitialData } from "../libs/initialSetup/initialSetup";
-import { uptdateEmailToObject } from "../utils/updateUsers";
 
 const URI = process.env.MONGODB_URI || "";
 export default async function main() {
@@ -8,7 +7,6 @@ export default async function main() {
   await mongoose.connect(URI);
   console.log("database is connected");
   await createInitialData();
-  await uptdateEmailToObject();
 }
 
 main().catch((err) => console.log(err));
