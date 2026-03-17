e35f1d57a8809358e944ba6f4dda2020555cf3cf
brandon <brandonotzin@gmail.com>
2026-03-17 12:18:33 -0600

    add feacture: get verifications request

--- src/controllers/verification.ts
+++ src/controllers/verification.ts
@@ -11,25 +11,69 @@ export const createVerification = async (req: Request, res: Response) => {
     const { response, status } = await uploadVerificationDocumentsToS3(files, userId);
     if (status !== 200) return res.status(status).json(response);
 
-    const verificationSaved=await verificationModel.create({ userId,status:"pendiente", images: response });
-    await userModel.findByIdAndUpdate(userId,{$set:{verify: "pendiente",}})
-    
+    const verificationSaved = await verificationModel.create({ userId, status: "pendiente", images: response });
+    await userModel.findByIdAndUpdate(userId, { $set: { verify: "pendiente", } })
+
     res.status(201).json(verificationSaved);
   } catch (error) {
-    handleHttp(res, "Error_Create_Service", error);
+    handleHttp(res, "Error_Create_Verification", error);
   }
 };
 
-export const getStatusVerification=async(req:Request,res:Response)=>{
+export const getStatusVerification = async (req: Request, res: Response) => {
   const userId = req.userId;
   try {
-    const userFound=await userModel.findById(userId);
+    const userFound = await userModel.findById(userId);
     if (!userFound) {
       return res.status(404).json({ error: "404 user not found" });
     }
-    
-    res.status(201).json({status:userFound.verify});
+
+    res.status(201).json({ status: userFound.verify });
   } catch (error) {
-    handleHttp(res, "Error_Create_Service", error);
+    handleHttp(res, "Error_Get_Status_Verification", error);
   }
 }
+
+export const getPendindVerification = async (req: Request, res: Response) => {
+  /* paginate */
+  const limit = 10;
+  const queryPage = req.query.page ? `${req.query.page}` : "1";
+  let page = Number(queryPage);
+
+
+
+  try {
+    const verifications = await verificationModel
+      .find({ status: "pendiente" })
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .populate({
+        path: "userId",
+        select: "_id name photoUrl isCompany",
+        populate: {
+          path: "photoUrl",
+          select: "url",
+        },
+      })
+      .populate({
+        path: "images",
+        select: "url", // Especifica el campo que deseas recuperar
+      })
+
+
+    let totalDocs = await verificationModel.count({ status: "pendiente" }); //Possible performance improvement: cache the value
+    let totalPages = Math.ceil(totalDocs / limit); //Possible performance improvement: cache the value
+
+    return res.status(200).json({
+      docs: verifications,
+      currentPage: page,
+      limit,
+      totalDocs,
+      totalPages,
+    });
+  } catch (error) {
+    handleHttp(res, "Error_Get_Pending_Verification", error);
+  }
+};
+
+
--- src/routes/verification.ts
+++ src/routes/verification.ts
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import { isValidToken } from "../middlewares/verifyToken";
 import { uploadImagesVerification } from "../middlewares/multerVerification";
-import { createVerification, getStatusVerification } from "../controllers/verification";
+import { createVerification, getPendindVerification, getStatusVerification } from "../controllers/verification";
 import { isStatusAllowed } from "../middlewares/verification";
 const router = Router();
 
 router.post(
   "/",
   uploadImagesVerification.array("images"),
-  [isValidToken,isStatusAllowed],
+  [isValidToken, isStatusAllowed],
   createVerification
 );
 
-router.get("/status",isValidToken,getStatusVerification)
+router.get("/status", isValidToken, getStatusVerification);
+router.get("/verifications-request", isValidToken, getPendindVerification);
 
 export { router };
